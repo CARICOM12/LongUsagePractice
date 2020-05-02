94d0961bef7a9948bf3e67c1ffc27f14ef4df5b2
CARICOM12 <0190a@ukr.net>
2020-05-02 21:55:18 +0300

    Add files via upload

---
+++ LongUsagePractice/LongUsagePractice/Program.cs
@@ -0,0 +1,29 @@
+﻿/*using System;
+class Inches
+{
+    static void Main()
+    {
+        long inches;
+        long miles;
+        miles = 93000000; // 93,000,000 miles to the sun
+                          // 5,280 feet in a mile, 12 inches in a foot.
+        inches = miles * 5280 * 12;
+        Console.WriteLine("Distance to the sun: " +
+        inches + " inches.");
+    }
+}
+*/
+using System;
+class Inches
+{
+    static void Main()
+    {
+        long inches;
+        long miles;
+        miles = 93000000; // 93 000 000 миль до Солнца
+                          // 5 280 футов в миле, 12 дюймов в футе,
+        inches = miles * 5280 * 12;
+        Console.WriteLine("Расстояние до Солнца: " +
+        inches + " дюймов.");
+    }
+}
